68f36529ae6a257e6018600346d93affead34c50
unknown <JanSokolowski1995@gmail.com>
2025-09-01 20:34:31 +0200

    Delete start.sh file

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Start the FastAPI application in the background
-uvicorn main:app --host 0.0.0.0 --port 8000 &
-
-#Start the Streamlit UI in the foreground
-streamlit run streamlit_app.py --server.port 8501 --server.address 0.0.0.0
